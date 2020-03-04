a3bfaf3d5657fde5f368f7e973c056c22fc95edc
Jose <developer.b.uno@gmail.com>
2020-03-04 20:22:09 -0300

    Scenes , models

---
+++ DesafioBCI/Get API/Model/CancionModel.swift
@@ -0,0 +1,17 @@
+//
+//  CancionModel.swift
+//  DesafioBCI
+//
+//  Created by Jose on 04-03-20.
+//  Copyright © 2020 dev_Jo. All rights reserved.
+//
+
+import Foundation
+
+struct CancionModel {
+    
+    var trackName: String
+    var collectionId: Int
+    var artistName: String
+    
+}
